45aef2b8ba4ebdfc1419013110e2d1fbf0c122dc
ko-tarou <tkota6174@gmail.com>
2024-12-15 20:33:19 +0900

    治った

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.websarva.wings.android.aivy"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.websarva.wings.android.aivy"
