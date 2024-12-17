1158bd9ca66af7f6e8d5a1d2542c11e9d89a804c
ko-tarou <tkota6174@gmail.com>
2024-12-17 16:20:59 +0900

    ツール表示非表示設定

--- app/src/main/java/com/websarva/wings/android/aivy/ui/ViewPage.kt
+++ app/src/main/java/com/websarva/wings/android/aivy/ui/ViewPage.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Language
 import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.material3.Button
@@ -14,18 +17,27 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.websarva.wings.android.aivy.ui.ViewPageParts.Mail
 import com.websarva.wings.android.aivy.ui.ViewPageParts.Present
 import com.websarva.wings.android.aivy.ui.ViewPageParts.ViewHeader
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import com.websarva.wings.android.aivy.ui.ViewPageParts.Add
 
 
 @Composable
 fun ViewPage(navController:NavController){
+
+    var isToolbarVisible by remember { mutableStateOf(false) }
+
     Column {
         ViewHeader(navController = navController)
 
@@ -40,11 +52,17 @@ fun ViewPage(navController:NavController){
                 horizontalAlignment = Alignment.End,
                 verticalArrangement = Arrangement.Bottom
             ) {
-                Mail()
-                Present()
+                if(isToolbarVisible) {
+                    Mail()
+                    Present()
+
+                }else{
+                    Add(
+                        isToolbarVisible = isToolbarVisible,
+                        onTool = {isToolbarVisible = !isToolbarVisible}
+                    )
+                }
             }
         }
-
-
     }
 }
