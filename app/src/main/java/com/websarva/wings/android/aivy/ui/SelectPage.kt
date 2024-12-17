30041d81fbfcf810e055a38f3bbddb963334a83e
ko-tarou <tkota6174@gmail.com>
2024-12-17 15:55:46 +0900

    presentボタン関s寧

--- app/src/main/java/com/websarva/wings/android/aivy/ui/SelectPage.kt
+++ app/src/main/java/com/websarva/wings/android/aivy/ui/SelectPage.kt
@@ -1,18 +1,45 @@
 package com.websarva.wings.android.aivy.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Language
+import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.websarva.wings.android.aivy.ui.common.Header
 
 @Composable
 fun SelectPage(navController: NavController){
     Column {
-        Button(
-            onClick = {navController.navigate("viewpage")}
-        ) {
-            Text("go to view")
-        }
+        Header(
+            title = "",
+            actions = {
+                IconButton(
+                    onClick = {navController.navigate("viewpage")},
+                    modifier = Modifier.offset(y = -10.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Language,
+                        contentDescription = "Language Icon"
+                    )
+                }
+                IconButton(
+                    onClick = {navController.navigate("viewpage")},
+                    modifier = Modifier.offset(y = -10.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.MoreHoriz,
+                        contentDescription = "MoreHoriz Icon"
+                    )
+                }
+            }
+        )
     }
 }
--- app/src/main/java/com/websarva/wings/android/aivy/ui/ViewPage.kt
+++ app/src/main/java/com/websarva/wings/android/aivy/ui/ViewPage.kt
@@ -1,6 +1,10 @@
 package com.websarva.wings.android.aivy.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Language
@@ -10,10 +14,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.websarva.wings.android.aivy.ui.ViewPageParts.Present
 import com.websarva.wings.android.aivy.ui.common.Header
 
 @Composable
@@ -42,5 +48,23 @@ fun ViewPage(navController:NavController){
                 }
             }
         )
+
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            //toolbar
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                Present()
+            }
+        }
+
+
     }
 }
